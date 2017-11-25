1315d1b40b13c18635eec71373a7e8595c833a9f
Honza Javorek <mail@honzajavorek.cz>
2017-11-25 21:02:02 +0100

    test: Flush stdout as soon as it's printed

--- test_nightly.py
+++ test_nightly.py
@@ -2,11 +2,17 @@ import doctest
 import os
 import json
 import re
+import sys
 import time
 from datetime import datetime, timedelta
 
 import requests
 import lxml.html
+
+
+def log(*args, **kwargs):
+    print(*args, **kwargs)
+    sys.stdout.flush()
 
 
 def get_twitter_time_offset():
@@ -35,34 +41,34 @@ if __name__ == '__main__':
     assert 'PASSWORD' in os.environ, 'The PASSWORD environment variable needs to be set'
 
     # The time Twitter renders to their static HTML is (probably) in the San Franciso time zone
-    print('Getting Twitter time')
+    log('Getting Twitter time')
     twitter_td = get_twitter_time_offset()
 
     before_test_dt = datetime.now() + twitter_td
-    print(f'About to run the code example from README, {before_test_dt.isoformat()} (Twitter time)')
+    log(f'About to run the code example from README, {before_test_dt.isoformat()} (Twitter time)')
     doctest.testfile('README.rst')
-    print(f'The code example has been executed, waiting 4 minutes')
+    log(f'The code example has been executed, waiting 4 minutes')
     for i in range(4):
         time.sleep(60)
-        print(f'Minute #{i + 1} elapsed')
+        log(f'Minute #{i + 1} elapsed')
     after_test_dt = datetime.now() + twitter_td
-    print(f'About to do assertions, {after_test_dt.isoformat()} (Twitter time)')
+    log(f'About to do assertions, {after_test_dt.isoformat()} (Twitter time)')
 
     tweets = read_tweets()
     tweet2 = next(tweets)
     tweet1 = next(tweets)
 
-    print("The tweet #1 starts with 'First Tweet'")
+    log("The tweet #1 starts with 'First Tweet'")
     assert tweet1[0].startswith('First Tweet')
-    print("The tweet #1 is within the expected time range")
+    log("The tweet #1 is within the expected time range")
     assert before_test_dt < tweet1[1] < after_test_dt
 
-    print("The tweet #2 starts with 'Second Tweet'")
+    log("The tweet #2 starts with 'Second Tweet'")
     assert tweet2[0].startswith('Second Tweet')
-    print("The tweet #1 is within the expected time range")
+    log("The tweet #1 is within the expected time range")
     assert before_test_dt < tweet2[1] < after_test_dt
 
-    print('The time difference between last two tweets is not more than 1 min')
+    log('The time difference between last two tweets is not more than 1 min')
     assert int((tweet2[1] - tweet1[1]).seconds / 60) <= 1
 
-    print('Done!')
+    log('Done!')
